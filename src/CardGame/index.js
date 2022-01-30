38d5c597b73f95bcba7c9f136106eee10ae4123e
deborahsalves <salves.deborah@gmail.com>
2022-01-30 18:04:59 -0300

    Re-start project. Initial CardGame CSS

---
+++ src/CardGame/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+            Card game
+        </article>
+    `
+}
+
+export default CardGame;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CardGame from './CardGame';
+import './styles/settings/colours.css';
+import './styles/elements/base.css';
+import './styles/generic/reset.css';
+
+const $root = document.getElementById('root');
+const cardGame = CardGame();
+
+$root.insertAdjacentHTML('beforeend', cardGame);
+
