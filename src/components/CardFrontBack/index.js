31a20e8922f8671f73b7f1509f0adde3d640e483
deborahsalves <salves.deborah@gmail.com>
2022-02-11 09:32:13 -0300

    Add click event to CardFrontBack component

--- src/components/CardFrontBack/index.js
+++ src/components/CardFrontBack/index.js
@@ -2,8 +2,19 @@ import './index.css';
 import CardGame from '../CardGame/index'
 
 const CardFrontBack = () => {
+    /* in order to NOT overwrite it with the handleClick of another function
+    we create a NAMESPACE in the global scope to store this handleClick */
+    window.cardFrontBack = {};
+    // then just place the function in that namespace
+    window.cardFrontBack.handleClick = (event) => {
+        const $origin = event.target;
+        const $cardFrontBack = $origin.closest('.card-front-back');
+
+        $cardFrontBack.classList.toggle('-active');
+    }
+
     return `
-        <article class="card-front-back">
+        <article class="card-front-back" onclick="cardFrontBack.handleClick(event)">
             <div class="card -front">
                 ${CardGame()}
             </div>
