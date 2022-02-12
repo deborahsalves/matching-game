44705404527d5a6fab82f340f8effaaff964481b
deborahsalves <salves.deborah@gmail.com>
2022-02-12 17:57:20 -0300

    Add features to hide cards and swap players after a turn

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -12,14 +12,37 @@ const BoardGame = (amountCards) => {
         copiedArray.splice(rand, 1);
         return copiedArray.length === 0 ? randomisedArray : randomiseCards(copiedArray, randomisedArray);
     }
+
+    const flipAndHideCards = ($activeCards) => {
+        $activeCards.forEach((card) => card.classList.remove('-active'))
+    }
+
+    const swapCurrentPlayer = () => {
+        const $playerTurn = document.querySelector('.player-turn');
+        const currentPlayer = $playerTurn.getAttribute('data-currentPlayer');
+        $playerTurn.setAttribute('data-currentPlayer', currentPlayer == 1 ? 2 : 1);
+    }
+
+    window.boardGame = {};
+    window.boardGame.handleClick = () => {
+        const $boardGame = document.querySelector('.board-game');
+        const $activeCards = $boardGame.querySelectorAll('.card-front-back.-active');
+        if ($activeCards.length === 2) {
+            setTimeout(() => {
+                flipAndHideCards($activeCards);
+                swapCurrentPlayer();
+            }, 1000);
+        }
+
+    }
+
     const randomisedCards = randomiseCards(cards);
-    console.log(randomisedCards);
     const htmlCardsList = randomisedCards.map((card) => CardFrontBack(card.cardContent, card.cardAlt))
     // the $ symbols identifies renders elements
     const $htmlCards = htmlCardsList.join('');
 
     return `
-        <section class="board-game">
+        <section class="board-game" onClick="boardGame.handleClick()">
             ${$htmlCards}
         </section>`;
 
