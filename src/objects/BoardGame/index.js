23b43914951a3b35039c3132157fb63b5311497c
deborahsalves <salves.deborah@gmail.com>
2022-02-12 17:31:27 -0300

    Add randomizer to card data before displaying BoardGame

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -3,7 +3,18 @@ import cards from './data'
 import CardFrontBack from '../../components/CardFrontBack/index';
 
 const BoardGame = (amountCards) => {
-    const htmlCardsList = cards.map((card) => CardFrontBack(card.cardContent, card.cardAlt))
+    const randomiseCards = (dataArray, returnArray = []) => {
+        const randomisedArray = returnArray;
+        let copiedArray = dataArray;
+        const rand = Math.floor(Math.random() * copiedArray.length);
+        const pickedArrayItem = copiedArray[rand];
+        randomisedArray.push(pickedArrayItem);
+        copiedArray.splice(rand, 1);
+        return copiedArray.length === 0 ? randomisedArray : randomiseCards(copiedArray, randomisedArray);
+    }
+    const randomisedCards = randomiseCards(cards);
+    console.log(randomisedCards);
+    const htmlCardsList = randomisedCards.map((card) => CardFrontBack(card.cardContent, card.cardAlt))
     // the $ symbols identifies renders elements
     const $htmlCards = htmlCardsList.join('');
 
